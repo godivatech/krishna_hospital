08de77d1f44d2c9b34478c9f7e148822f43c94c9
godivatech <infogodivatech@gmail.com>
2026-04-25 13:33:04 +0530

    code changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -25,7 +25,7 @@ export default function Hero() {
       accent: "text-teal-200"
     },
     {
-      title: "Advanced",
+      title: "Specialized",
       subtitle: "ICU & NICU",
       description: "Providing the highest level of critical care and advanced life support for adult and neonatal patients in a safe environment.",
       image: "/banner_3.png",
